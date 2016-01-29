6d5b1cf3d3ea1bfe83a1b87504ee40f743762460
danwheeler <da-wheeler@hotmail.co.uk>
2016-01-29 11:27:36 +0000

    angular login and logout working ng-show/ hide need work

--- app/assets/javascripts/controllers/userController.js
+++ app/assets/javascripts/controllers/userController.js
@@ -1,10 +1,69 @@
 'use strict';
 
-angular.module('StockOrderApp.controllers').controller('UserController', ['$http', '$location', function($http, $location, Auth) {
+angular.module('StockOrderApp.controllers').controller('UserController', ['$http', '$location', 'Auth', '$scope', function($http, $location, Auth, $scope) {
 	var self = this;
-  // self.path = 'http://localhost:3000/users';
-
+	self.path = 'http://localhost:3000/users';
+	self.auth = Auth;
 	console.log('loaded u');
+
+	// $http.get(self.path)
+	// 	.then( function (response) {
+	// 		self.users = response.data.users;
+	// 		console.log(self.users);
+	// });
+
+	// self.show = function () {
+	// 	$http.get(self.path)
+	// 	.then(function (response) {
+	// 		self.users = response.data.users;
+	// 	});
+	// };
+
+	// self.create = function () {
+	// 	var data = {
+	// 		email: self.email,
+	// 		password: self.password
+	// 	};
+	// 	$http.post(self.path, data)
+	// 	.then(function() {
+	// 		self.show();
+	// 		$location.path('/users');
+	// 	});
+	// };
+
+	// self.update = function (data, id) {
+	// 	$http.put(self.path + id, data)
+	// 	.then(function(response) {
+	// 		self.resultPut = response;
+	// 		$location.path('/users');
+	// 	});
+	// };
+
+	// self.delete = function (id) {
+	// 	$http.delete(self.path + id)
+	// 	.then(function() {
+	// 		self.show();
+	// 	});
+	// };
+
+	// self.login = function (data, id) {
+	// 	$http.put(self.path + '/sign_in', data)
+	// 	.then(function(response) {
+	// 		self.resultPut = response;
+	// 		$location.path('/users');
+	// 	});
+	// };
+
+	// self.logout = function (id) {
+	// 	$http.delete(self.path + '/sign_out' + id)
+	// 	.then(function() {
+	// 		self.show();
+	// 	});
+	// };
+
+	self.loggenIn = function() {
+		return self.auth.isAuthenticated();
+	};
 
 	self.login = function() {
 	  var credentials = {
@@ -22,14 +81,12 @@ angular.module('StockOrderApp.controllers').controller('UserController', ['$http
         console.log(user); // => {id: 1, ect: '...'}
     }, function(error) {
         console.log("failed")
-    });
-
-    self.$on('devise:login', function(event, currentUser) {
+    })
+    .then('devise:login', function(event, currentUser) {
         // after a login, a hard refresh, a new tab
         self.currentUser = Auth._currentUser.email
-    });
-
-    self.$on('devise:new-session', function(event, currentUser) {
+    })
+    .then('devise:new-session', function(event, currentUser) {
         // user logged in by Auth.login({...})
         self.currentUser = Auth._currentUser.email
     });
@@ -45,7 +102,7 @@ angular.module('StockOrderApp.controllers').controller('UserController', ['$http
     var oldUser = self.currentUser;
  		
  		Auth.logout(config1).then(function(oldUser) {
- 			self.currentUser = "";
+ 			$scope.currentUser = "";
  			console.log(oldUser.email)
       alert(oldUser.email + " you're signed out now.");
         
@@ -76,7 +133,7 @@ angular.module('StockOrderApp.controllers').controller('UserController', ['$http
         console.log("failed")
     });
 
-    self.$on('devise:register', function(event, currentUser) {
+    $scope.$on('devise:register', function(event, currentUser) {
         // after a register, a hard refresh, a new tab
         self.currentUser = Auth._currentUser.email
     });
