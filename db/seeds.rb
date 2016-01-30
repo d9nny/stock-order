8a70a3ce588928a1f6549e4559c627d0402e116e
danwheeler <da-wheeler@hotmail.co.uk>
2016-01-30 11:53:04 +0000

    added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# Company.create!(name: "Sainsburys", budget:1000)
-# Company.create!(name: "Tugo", budget:2000)
-# Company.create!(name: "Tuco", budget:3000)
-# User.create!(email: "dan@dan.com", password: "Password1")
-# User.create!(email: "ed@ed.com", password: "Password2")
-# User.create!(email: "tom@tom.com", password: "Password3")
-
+Company.create!(name: "Sainsburys", budget:1000)
+Company.create!(name: "Tugo", budget:2000)
+Company.create!(name: "Tuco", budget:3000)
+User.create!(email: "dan@dan.com", password: "Password1")
+User.create!(email: "ed@ed.com", password: "Password2")
+User.create!(email: "tom@tom.com", password: "Password3")
 User.create!(email: "admin@admin.com", password: "Password1", admin: true)
